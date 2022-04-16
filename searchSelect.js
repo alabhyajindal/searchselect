1ddcd25b5024b5716ee47227a32f7b32267965c9
Alabhya Jindal <alabhya10@gmail.com>
2022-04-16 19:16:42 +0530

    removed eventListener for DOMContentLoaded for faster testing, add back later if needed

--- searchSelect.js
+++ searchSelect.js
@@ -1,43 +1,38 @@
-// Classes applied to the search results on Google
-// LC20lb MBeuO DKV0Md
+// Array of all the search results shown by Google
+let resultsLinks = Array.from(
+  document.querySelectorAll('.LC20lb.MBeuO.DKV0Md')
+);
+// Links which are shown by Google in the "People also ask" section
+let askBox = document.querySelector('.ULSxyf');
+// Array of links in the askBox section
+let askBoxLinks = Array.from(askBox.querySelectorAll('.LC20lb.MBeuO.DKV0Md'));
 
-addEventListener('DOMContentLoaded', () => {
-  // Array of all the search results shown by Google
-  let resultsLinks = Array.from(
-    document.querySelectorAll('.LC20lb.MBeuO.DKV0Md')
-  );
-  // Links which are shown by Google in the "People also ask" section
-  let askBox = document.querySelector('.ULSxyf');
-  // Array of links in the askBox section
-  let askBoxLinks = Array.from(askBox.querySelectorAll('.LC20lb.MBeuO.DKV0Md'));
+// Decalaring an empty array to hold all resultLinks minus askBoxLinks
+let mainLinks = [];
 
-  // Decalaring an empty array to hold all resultLinks minus askBoxLinks
-  let mainLinks = [];
-
-  for (let rLink of resultsLinks) {
-    let count = 0;
-    for (let aLink of askBoxLinks) {
-      if (rLink !== aLink) {
-        count++;
-      }
-    }
-    if (count === askBoxLinks.length) {
-      mainLinks.push(rLink);
+for (let rLink of resultsLinks) {
+  let count = 0;
+  for (let aLink of askBoxLinks) {
+    if (rLink !== aLink) {
+      count++;
     }
   }
+  if (count === askBoxLinks.length) {
+    mainLinks.push(rLink);
+  }
+}
 
-  // Initializing a variable to help cycle between the mainLinks
-  let currentLink = 0;
-  // Event listener to listen for the keyup event of the Ctrl key
-  window.addEventListener('keyup', (e) => {
-    if (e.key === 'Control') {
-      e.preventDefault();
-      mainLinks.forEach((link) => {
-        link === mainLinks[currentLink]
-          ? (link.style.backgroundColor = 'green')
-          : (link.style.backgroundColor = 'white');
-      });
-      currentLink++;
-    }
-  });
+// Initializing a variable to help cycle between the mainLinks
+let currentLink = 0;
+// Event listener to listen for the keyup event of the Ctrl key
+window.addEventListener('keyup', (e) => {
+  if (e.key === 'Control') {
+    e.preventDefault();
+    mainLinks.forEach((link) => {
+      link === mainLinks[currentLink]
+        ? (link.style.backgroundColor = 'green')
+        : (link.style.backgroundColor = 'white');
+    });
+    currentLink++;
+  }
 });
