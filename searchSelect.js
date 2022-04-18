b29ab3244a009db88c0441b4f454e1eb44793b7c
Alabhya Jindal <alabhya10@gmail.com>
2022-04-18 21:24:05 +0530

    Conditionally removing Ask Box links from resultLinks
    Removing links from Knowledge Panel
    
    Circling back to the first link after reaching the end

--- searchSelect.js
+++ searchSelect.js
@@ -4,30 +4,58 @@ let resultsLinks = Array.from(
 );
 // Links which are shown by Google in the "People also ask" section
 let askBox = document.querySelector('.ULSxyf');
+let askBoxLinks;
 // Array of links in the askBox section
-let askBoxLinks = Array.from(askBox.querySelectorAll('.LC20lb.MBeuO.DKV0Md'));
+if (askBox) {
+  askBoxLinks = Array.from(askBox.querySelectorAll('.LC20lb.MBeuO.DKV0Md'));
+}
 
-// Decalaring an empty array to hold all resultLinks minus askBoxLinks
+// Links which are shown by Google in the "Knowledge Panel" section
+let knowledgePanel = document.querySelector('.I6TXqe');
+let knowledgePanelLinks;
+// Array of links in the Knowledge Panel section
+if (knowledgePanel) {
+  knowledgePanelLinks = Array.from(
+    knowledgePanel.querySelectorAll('.LC20lb.MBeuO.DKV0Md')
+  );
+}
+
+// Decalaring an empty array to hold the iterable array of links
 let mainLinks = [];
 
-for (let rLink of resultsLinks) {
-  let count = 0;
-  for (let aLink of askBoxLinks) {
-    if (rLink !== aLink) {
-      count++;
+// Removing the askBoxLinks from the resultsLinks
+if (askBoxLinks.length > 0) {
+  for (let rLink of resultsLinks) {
+    let count = 0;
+    for (let aLink of askBoxLinks) {
+      if (rLink !== aLink) {
+        count++;
+      }
+    }
+    if (count === askBoxLinks.length) {
+      mainLinks.push(rLink);
     }
   }
-  if (count === askBoxLinks.length) {
-    mainLinks.push(rLink);
+} else {
+  mainLinks = [...resultsLinks];
+}
+
+// Removing a link from the mainLinks array if the link is found in the knowledgePanelSection as well
+let temp = [];
+for (let link of mainLinks) {
+  if (!knowledgePanelLinks.includes(link)) {
+    temp.push(link);
   }
 }
+mainLinks = [...temp];
 
 // Initializing a variable to help cycle between the mainLinks
 let currentLink = 0;
-// Event listener to listen for the keyup event of the Ctrl key
+// Event listener to listen for the keyup event of the Control key
 window.addEventListener('keyup', (e) => {
   // Circles back to the first link the user reaches the last link on the search page
   if (currentLink > mainLinks.length - 1) currentLink = 0;
+  // Removing focus from the selected link when the user goes back to the search bar
   if (e.key == '/') {
     mainLinks[currentLink - 1].style.textDecoration = 'none';
     currentLink = 0;
@@ -37,7 +65,9 @@ window.addEventListener('keyup', (e) => {
     e.preventDefault();
     mainLinks.forEach((link) => {
       if (link === mainLinks[currentLink]) {
+        // Visual indication that the link is in focus
         link.style.textDecoration = 'underline';
+        // Smooth scrolling the selected link to the center of the screen
         link.scrollIntoView({
           behavior: 'smooth',
           block: 'center',
