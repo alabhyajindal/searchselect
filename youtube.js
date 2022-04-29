d766088c8a09f2713dc0791abff442003910b0e9
Alabhya Jindal <alabhya10@gmail.com>
2022-04-29 17:55:58 +0530

    Started working on YouTube

---
+++ youtube.js
@@ -0,0 +1,6 @@
+// let videoList = document.querySelectorAll('img#style-scope#yt-img-shadow');
+// videoList.forEach((video) => (video.style.opacity = 0.5));
+
+let videoList = document.querySelectorAll(
+  '#video-title.ytd-playlist-renderer, #video-title.ytd-video-renderer'
+);
